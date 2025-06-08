2367a42722d96e2c5227d8a845797e1d2684851b
Felipe Silva Coelho <111004133+fs-coelho@users.noreply.github.com>
2025-06-08 14:39:16 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// contador
+let contador = document.getElementById("contador");
+
+// botao add
+let botaoAdd = document.getElementById("addTask");
+
+// botao check
+let botaoCheck = document.getElementById("check");
+
+// botao remover
+let botaoRemover = document.getElementById("deletar");
